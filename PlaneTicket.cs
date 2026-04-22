c2105613e6c627227cced0f2379172506fa99a76
Frizz-NT <anggodo945@gmail.com>
2026-04-22 16:53:55 +0700

    Add files via upload

---
+++ PlaneTicket.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTicket
+    {
+        // Field 
+        private string _origin;
+        private string _destination;
+        private double _cost; 
+        private string _currency;
+
+        // Properties
+        public string Origin
+        {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+
+        public string Destination
+        {
+            get { return _destination; }
+            set { _destination = value; }
+        }
+
+        public double Cost
+        {
+            get { return _cost; }
+            set { _cost = value; }
+        }
+
+        public string Currency
+        {
+            get { return _currency; }
+            set { _currency = value; }
+        }
+
+        // Method
+        public void PrintSummary()
+        {
+            Console.WriteLine($"Origin: {Origin}");
+            Console.WriteLine($"Destination: {Destination}");
+            Console.WriteLine($"Cost: {Cost} ({Currency})");
+
+            // Logika konversi mata uang
+            if (Currency.ToUpper() == "USD")
+            {
+                // Karena Cost sudah double, perkalian ini sekarang valid
+                double convertToRupiah = Cost * 15000;
+                Console.WriteLine($"Convert to Rupiah: {convertToRupiah}");
+            }
+            else if (Currency.ToUpper() == "IDR")
+            {
+                // Memperbaiki typo 'cosst' menjadi 'Cost'
+                double convertToDolar = Cost / 15000;
+                Console.WriteLine($"Convert to Dolar: {convertToDolar}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek pertama (ticket1)
+            PlaneTicket ticket1 = new PlaneTicket();
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            // Membuat objek kedua (ticket2)
+            PlaneTicket ticket2 = new PlaneTicket();
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            // Memanggil method PrintSummary untuk menampilkan output
+            ticket1.PrintSummary();
+            ticket2.PrintSummary();
+
+            // Agar console tidak langsung tertutup
+            Console.WriteLine("Tekan sembarang tombol untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+}
